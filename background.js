f173c46ee51b778ea0c0c0b2613b85116741e6d6
Daniel Stepanov <danielstepanov@Daniels-MacBook-Pro-2.local>
2014-07-10 21:44:09 -0400

    all deez nuts

--- background.js
+++ background.js
@@ -2,15 +2,14 @@ var green = 0;
 var red = 1;
 var current = green;
 
-/*(document).ready(function(){
+$(document).ready(function(){
    var audio = "<audio id = 'myAudio'> 
  <source src ="hellyeah.mp3" type ="audio/mpeg"> </audio";
-   ('body').append(audio);
-   ("a:contains('Hellllllll yeah')").click(function(){
-   ("#myAudio").play();
+   $('body').append(audio);
+   $("a:contains('Hellllllll yeah')").click(function(){
+   $("#myAudio").play();
 });
-}); */
-
+});
 
 function updateIcon() {
 if (current == red) {
